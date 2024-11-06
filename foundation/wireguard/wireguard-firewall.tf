a534db161d7af93f5bfda261dff91ea524b8fc0c
Redjol Resulaj <redjol.resulaj@yahoo.com>
2024-11-06 11:28:49 +0100

    Fixed wireguard firewall rules

--- foundation/wireguard/wireguard-firewall.tf
+++ foundation/wireguard/wireguard-firewall.tf
@@ -5,8 +5,33 @@ resource "hcloud_firewall" "wireguard_firewall" {
 
   rule {
     direction       = "out"
+    protocol        = "tcp"
+    port            = "1-65535"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction       = "out"
     protocol        = "udp"
-    port            = "51820"
+    port            = "1-65535"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction       = "out"
+    protocol        = "icmp"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction       = "out"
+    protocol        = "gre"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction       = "out"
+    protocol        = "esp"
     destination_ips = ["0.0.0.0/0", "::/0"]
   }
 
